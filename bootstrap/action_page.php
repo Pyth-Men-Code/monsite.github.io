1b087837a98e8831711ac35c941f2558cd9fcd83
sof99-web <57967264+sof99-web@users.noreply.github.com>
2020-05-04 00:58:51 +0200

    Add files via upload

---
+++ bootstrap/action_page.php
@@ -0,0 +1,39 @@
+ 
+ <?php
+
+
+//ouverture d'une connexion à la bdd monsite
+
+    $objetpdo  = new pdo ('mysql:host=localhost;dbname=monsite','root','');
+    //prépation d'une requete 
+    $objectpdo->prepare('INSERT INTO interlocuteur values (null, :nom  ,:prenom  ,:password  ,:email)');
+    //marquation
+    $pdoStat->bindValues(':nom',$_POST['nom'], PDO::PARAM_STR);
+    $pdoStat->bindValues(':prernom',$_POST['prenom'], PDO::PARAM_STR);
+    $pdoStat->bindValues(':password',$_POST['password'], PDO::PARAM_STR);
+    $pdoStat->bindValues(':email',$_POST['email'], PDO::PARAM_STR);
+    //execution de la requete 
+    $insertIsOk =  insertisok $pdoStat->execute();
+
+
+    if($insertIsOk){
+        $message ='le contact a été enregistré';
+    }
+    else{
+        $message='le contact na pas été enregistré';
+
+    }
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+      <h1> insertion des contact</p> 
+      <p><?php echo $message; ?> </p>
+
+    </body>
+    </html>
---
+++ bootstrap/js/sofsite.js
@@ -0,0 +1,21 @@
+var add_pwd= document.getElementById("email");
+if(add_pwd.value=="soufiane1999bel@gmail.com")
+alert("rrtgfgj,hnj,  ");
+else
+alert("adresse correcte");
+/*function verifier(){
+    if( document.getElementById("pwd").value.indexOf("sof")==-1) (
+
+    document.getElementById("pwd").className="phraseerronee"
+    //document.getElementById("salah").setAttribute("style","border: 1px solid red");
+    //document.getElementById("salah").setAttribute("class","phraseerronee");
+    alert("incorrect passeword");
+    )
+    else(
+        
+    document.getElementById("pwd").className="phrasecorrecte"
+    //document.getElementById("salah").setAttribute("style","border: 1px solid green");
+    //document.getElementById("salah").setAttribute("class","phrasecorrecte");
+    alert("correct passeword");
+    )
+}*/
